47927791bc938664605ee80f97dfee1ed6edb4fb
Cyberunner23 <narutoxela@gmail.com>
2015-12-07 23:32:41 -0500

    Cleanup.

--- src/config.rs
+++ src/config.rs
@@ -1,36 +1,36 @@
 
 use std::env;
-use std::fs::{OpenOptions};
+use std::fs::{File, Metadata, OpenOptions};
+use std::io::Read;
 use std::path::{Path, PathBuf};
+
+extern crate rustc_serialize;
+use rustc_serialize::json;
 
 
 pub struct TrelloBSTConfigPath {
     pub config_path: PathBuf
 }
 
-#[derive(RustcDecodable, RustcEncodable)]
-pub struct TrelloBSTAPIConfig {
-    pub trello_api_key:       String,
-    pub trello_app_key:       String,
-    pub travis_access_token: String,
-    pub appveyor_api_token:  String,
-}
-
-
 impl TrelloBSTConfigPath {
+
+    pub fn new() -> TrelloBSTConfigPath {
+        TrelloBSTConfigPath {
+            config_path: PathBuf::new(),
+        }
+    }
 
     pub fn try_default_config_path() -> Result<TrelloBSTConfigPath, &'static str> {
 
         //Check for an existing config file
 
-        let mut _home_dir:       PathBuf;
-        let mut _home_dir_found: bool;
-        _home_dir = PathBuf::new();
+        let mut trellobst_config_path = TrelloBSTConfigPath::new();
+        let mut _home_dir_found:        bool;
 
         match env::home_dir() {
             Some(home_dir) => {
-                _home_dir       = home_dir;
-                _home_dir_found = true
+                trellobst_config_path.config_path = home_dir;
+                _home_dir_found                   = true;
             },
             None           => {
                 _home_dir_found = false;
@@ -38,13 +38,13 @@ impl TrelloBSTConfigPath {
         };
 
         if _home_dir_found {
-            _home_dir.push(".TrelloBST.cfg");
+            trellobst_config_path.config_path.push(".TrelloBST.cfg");
             //Try to open the file with r/w permissions
-            match OpenOptions::new().read(true).write(true).create(true).open(_home_dir.as_path()) {
-                Ok(_) => {
-                    Ok(TrelloBSTConfigPath{config_path: PathBuf::from(_home_dir)})
+            match OpenOptions::new().read(true).write(true).create(true).open(trellobst_config_path.config_path.as_path()) {
+                Ok(_)  => {
+                    Ok(trellobst_config_path)
                 }
-                Err(_)           => {
+                Err(_) => {
                     Err("Cannot open/create configuration file at default location.")
                 }
             }
@@ -68,7 +68,55 @@ impl TrelloBSTConfigPath {
 }
 
 
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct TrelloBSTAPIConfig {
+    pub trello_api_key:      String,
+    pub trello_app_key:      String,
+    pub travis_access_token: String,
+    pub appveyor_api_token:  String,
+}
 
+impl TrelloBSTAPIConfig {
+
+    pub fn new() -> TrelloBSTAPIConfig {
+        TrelloBSTAPIConfig {
+            trello_api_key:      "".to_string(),
+            trello_app_key:      "".to_string(),
+            travis_access_token: "".to_string(),
+            appveyor_api_token:  "".to_string(),
+        }
+    }
+
+    pub fn parse_from_file(file: &mut File) -> Result<TrelloBSTAPIConfig, &'static str>{
+        let metadata: Metadata;
+        match file.metadata() {
+            Ok(_metadata)  => {
+                metadata = _metadata;
+            }
+            Err(_) => {
+                return Err("Cannot gather metadata of the configuration file, configuration file won't be used...")
+            }
+        }
+
+        let api_config         = TrelloBSTAPIConfig::new();
+        let file_length: usize = metadata.len() as usize;
+
+        if file_length == 0 {
+            return Ok(api_config)
+        } else {
+            let mut data: String = String::with_capacity(file_length +1);
+            match file.read_to_string(&mut data) {
+                Ok(_) => {
+                    //TODO: better error checking
+                    return Ok(json::decode(&data[..]).unwrap())
+                },
+                Err(_) => {
+                    return Err("Error while reading the configuration file, configuration file won't be used...")
+                }
+            }
+        }
+    }
+}
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 
-use std::fs::{File, Metadata};
-use std::io::Read;
-use std::path::{Path, PathBuf};
+use std::fs::File;
+use std::path::Path;
 use std::process::exit;
 
+
 extern crate clap;
+use clap::{Arg, App};
+
 extern crate rustc_serialize;
 
-use clap::{Arg, App};
-use rustc_serialize::json;
 
 mod appveyor;
 mod config;
@@ -16,38 +16,46 @@ mod travis_ci;
 mod trello;
 
 
-fn parse_config_file(file: &mut File) -> Result<config::TrelloBSTAPIConfig, &'static str>{
-    //TODO: make array, read, parse, return config struct.
-    let metadata: Metadata;
-    match file.metadata() {
-        Ok(_metadata)  => {
-            metadata = _metadata;
-        }
-        Err(_) => {
-            return Err("Cannot gather metadata of the configuration file, configuration file won't be used...")
-        }
-    }
-
-    let file_length:   usize = metadata.len() as usize;
-    let api_config   = config::TrelloBSTAPIConfig {
-        trello_api_key:      "".to_string(),
-        trello_app_key:      "".to_string(),
-        travis_access_token: "".to_string(),
-        appveyor_api_token:  "".to_string()
-    };
-
-    if file_length == 0 {
-        return Ok(api_config)
-    } else {
-        let mut data: String = String::with_capacity(file_length +1);
-        match file.read_to_string(&mut data) {
-            Ok(_) => {
-                //TODO: better error checking
-                return Ok(json::decode(&data[..]).unwrap())
+fn get_config_path(is_using_custom_config: &mut bool, path_str: &str) -> Result<config::TrelloBSTConfigPath, &'static str> {
+    if *is_using_custom_config {
+        println!("Looking for the configuration file: {}", path_str);
+        let path = Path::new(path_str);
+        match config::TrelloBSTConfigPath::try_custom_config_path(path) {
+            Ok(_config) => {
+                return Ok(_config)
             },
-            Err(_) => {
-                return Err("Error while reading the configuration file, configuration file won't be used...")
+            Err(c) => {
+                return Err(c)
+            },
+        };
+    } else {
+        println!("Looking for the configuration file in default location...");
+        match config::TrelloBSTConfigPath::try_default_config_path() {
+            Ok(_config) => {
+                return Ok(_config)
+            },
+            Err(c) => {
+                return Err(c)
+            },
+        };
+    }
+}
+
+fn parse_config(config_path: config::TrelloBSTConfigPath) -> Result<config::TrelloBSTAPIConfig, &'static str>{
+    match File::open(config_path.config_path.as_path()) {
+        Ok(file) => {
+            let mut config_file = file;
+            match config::TrelloBSTAPIConfig::parse_from_file(&mut config_file) {
+                Ok(_config) => {
+                    return Ok(_config)
+                }
+                Err(err)   => {
+                    return Err(err)
+                }
             }
+        }
+        Err(_)   =>{
+            return Err("Cannot open config file for parsing, configuration file won't be used...");
         }
     }
 }
@@ -102,39 +110,20 @@ fn main() {
     //                   Get config path                      //
     ////////////////////////////////////////////////////////////
 
-    let mut config_path = config::TrelloBSTConfigPath {
-        config_path: PathBuf::new()
-    };
+    let mut config_path = config::TrelloBSTConfigPath::new();
 
-    if is_using_config_file && is_using_custom_config {
-        println!("Looking for the configuration file: {}", matches.value_of("CONFIG").unwrap());
-        let path = Path::new(matches.value_of("CONFIG").unwrap());
-        match config::TrelloBSTConfigPath::try_custom_config_path(path) {
-            Ok(_config) => {
-                config_path = _config;
-                println!("Found.");
-                is_using_config_file = true;
-            },
-            Err(c) => {
-                println!("An error occured: {}", c);
+    if is_using_config_file {
+        match get_config_path(&mut is_using_custom_config, matches.value_of("CONFIG").unwrap()) {
+            Ok(config) => {
+                config_path = config;
+                println!("Found.")
+            }
+            Err(err)=>{
+                println!("An error occurred: {}", err);
                 println!("Configuration file won't be used...");
                 is_using_config_file = false;
-            },
-        };
-    } else if is_using_config_file && !is_using_custom_config {
-        println!("Looking for the configuration file in default location...");
-        match config::TrelloBSTConfigPath::try_default_config_path() {
-            Ok(_config) => {
-                config_path = _config;
-                println!("Found.");
-                is_using_config_file = true;
-            },
-            Err(c) => {
-                println!("An error occured: {}", c);
-                println!("Configuration file won't be used...");
-                is_using_config_file = false;
-            },
-        };
+            }
+        }
     }
 
 
@@ -142,35 +131,23 @@ fn main() {
     //                     Parse config                       //
     ////////////////////////////////////////////////////////////
 
-    let     config: config::TrelloBSTAPIConfig;
-    let mut config_file: File;
+    let mut config = config::TrelloBSTAPIConfig::new();
 
     if is_using_config_file {
-
         println!("Parsing...");
-
-        match File::open(config_path.config_path.as_path()) {
-            Ok(file) => {
-                config_file = file;
-                match parse_config_file(&mut config_file) {
-                    Ok(_config) => {
-                        config = _config;
-                    }
-                    Err(err)   => {
-                        println!("{}", err);
-                        is_using_config_file = false;
-                    }
-                }
+        match parse_config(config_path) {
+            Ok(_config) => {
+                config = _config;
+                println!("Done.");
             }
-            Err(_)   =>{
-                println!("Cannot open config file for parsing, configuration file won't be used...");
+            Err(err)   => {
+                println!("{}", err);
                 is_using_config_file = false;
             }
         }
-
-        println!("Done.");
-
     }
+
+
 
 
 }
